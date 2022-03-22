994a20717c9cbf913b17cd71b4b684fa8053386d
RedHeadOnDuty <puddingwackel06@gmail.com>
2022-03-22 08:45:33 +0100

    Update VierGewinnt.java

--- src/Exersice1/VierGewinnt.java
+++ src/Exersice1/VierGewinnt.java
@@ -1,4 +1,29 @@
 package Exersice1;
 
 public class VierGewinnt {
+
+    static char Player1 = 'X';
+    static char Player2 = 'O';
+    static int rowCount = 5;
+    static int columnCount = 6;
+    static char[][] gameGrid = new char[5][6];
+
+    static void playerMove(char player){
+
+    }
+    static void paintGrid(){
+
+    }
+
+
+    public static void main(String[] args) {
+
+        //before game start
+
+        //game loop
+        while(true){
+            playerMove();
+            playerMove();
+        }
+    }
 }
