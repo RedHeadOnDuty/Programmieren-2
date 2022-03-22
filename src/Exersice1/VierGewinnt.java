fb0262c6c08bd4d0e72dbc2eb616bc512eb50248
RedHeadOnDuty <puddingwackel06@gmail.com>
2022-03-22 10:02:31 +0100

    Update VierGewinnt.java

--- src/Exersice1/VierGewinnt.java
+++ src/Exersice1/VierGewinnt.java
@@ -1,14 +1,31 @@
 package Exersice1;
 
+import java.util.Scanner;
+
 public class VierGewinnt {
 
     static char Player1 = 'X';
     static char Player2 = 'O';
-    static int rowCount = 5;
-    static int columnCount = 6;
-    static char[][] gameGrid = new char[5][6];
+    static int rowCount = 6;
+    static int columnCount = 7;
+    static char[][] gameGrid = new char[rowCount][columnCount];
 
     static void playerMove(char player){
+        Scanner sc = new Scanner(System.in);
+        char input;
+        do{
+            input = sc.next().charAt(0);
+        }while (input < '0' || input > '6');
+        int inputIndex = input - 48;
+        for(int i = rowCount - 1; i >= 0; i--){
+            if (gameGrid[i][inputIndex] == '.')
+            {
+                gameGrid[i][inputIndex] = player;
+                break;
+            }
+        }
+
+        //
 
     }
     static void paintGrid(){
@@ -37,6 +54,7 @@ public class VierGewinnt {
         while(true){
             paintGrid();
             playerMove(Player1);
+            paintGrid();
             playerMove(Player2);
         }
     }
