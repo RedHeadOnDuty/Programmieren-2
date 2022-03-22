0d698e5005233a909a219cf5fd0fabb5ed48d305
RedHeadOnDuty <puddingwackel06@gmail.com>
2022-03-22 09:16:32 +0100

    Update VierGewinnt.java

--- src/Exersice1/VierGewinnt.java
+++ src/Exersice1/VierGewinnt.java
@@ -12,18 +12,31 @@ public class VierGewinnt {
 
     }
     static void paintGrid(){
-
+        for (int i = 0; i < columnCount; i++){
+            System.out.printf("%02d ", i);
+        }
+        System.out.println();
+        for (char[] row:gameGrid) {
+            for (char c:row) {
+                System.out.printf("%c  ", c);
+            }
+            System.out.println();
+        }
     }
 
 
     public static void main(String[] args) {
-
         //before game start
-
+        for (int i = 0; i < rowCount; i++){
+            for(int j = 0; j < columnCount;j++){
+                gameGrid[i][j] = '.';
+            }
+        }
+        paintGrid();
         //game loop
         while(true){
-            playerMove();
-            playerMove();
+            playerMove(Player1);
+            playerMove(Player2);
         }
     }
 }
